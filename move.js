0f87c1f7ddb7d2647828331589fc1d2a1fde0604
Angie DeSchutter <adeschutter6437@gmail.com>
2023-02-28 16:01:28 -0800

    added js callback functions to move character

--- move.js
+++ move.js
@@ -6,7 +6,60 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
+    function moveWithArrowKeys(left, bottom, callback){
+        let direction = null;
+        let x = 100;
+        let y = 250;
+
+        function moveCharacter(){ 
+            if(direction === 'west' && x>=0){
+                x = x - 1
+            }
+            if(direction === 'north' && y<window.innerHeight-75){
+                y = y + 1
+            }
+            if(direction === 'east' && x<window.innerWidth-50){
+                x = x + 1
+            }
+            if(direction === 'south' && y>=0){
+                y = y - 1
+            }
+            character.style.left = x + 'px'
+            character.style.bottom = y + 'px'          
+        }   
+        setInterval(moveCharacter, 1)               
+                                       
+        document.addEventListener('keydown', function(e){
+            if(e.repeat) return;
+
+            if(e.key === 'ArrowLeft'){
+            direction = 'west'
+            }
+            if(e.key === 'ArrowUp'){
+            direction = 'north'
+            }
+            if(e.key === 'ArrowRight'){
+            direction = 'east'
+            }
+            if(e.key === 'ArrowDown'){
+            direction = 'south'
+            }
+            callback(direction)
+        })
+
+        document.addEventListener('keyup', function(e){
+        direction = null
+        callback(direction)
+        })
+    }
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
     }
 }
+
+
+
+
+
+
